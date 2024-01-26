d0f46351ed3ffab86aac6ad30288b99744a20d9b
Ismail Marzuqi <marzhochi@gmail.com>
2024-01-26 23:19:56 +0700

    update

--- database/migrations/2024_01_19_000009_create_tugas_table.php
+++ database/migrations/2024_01_19_000009_create_tugas_table.php
@@ -10,7 +10,7 @@ class CreateTugasTable extends Migration
     {
         Schema::create('tugas', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->tinyInteger('kategori')->default(1);
+            $table->string('kategori');
             $table->string('judul_tugas');
             $table->string('keterangan')->nullable();
             $table->tinyInteger('status')->default(1);
