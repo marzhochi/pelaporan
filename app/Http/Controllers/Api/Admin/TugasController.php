0aef280f4e519447c9340e128b0576e86ccc8be6
Ismail Marzuqi <marzhochi@gmail.com>
2024-02-12 22:19:13 +0700

    update

--- app/Http/Controllers/Api/Admin/TugasController.php
+++ app/Http/Controllers/Api/Admin/TugasController.php
@@ -77,7 +77,8 @@ class TugasController extends Controller
         $tugas->lokasi_id = $request->lokasi_id;
         $tugas->save();
 
-        $tugas->petugas()->attach($request->keterangan);
+        $tugas->petugas()->sync($request->input('petugas', []));
+        // $tugas->petugas()->attach($request->petugas);
 
         return response()->json([
             'status' => 'success',
