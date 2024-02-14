e9748a12df667d25303f96e6fc9cd138243fc8f1
Ismail Marzuqi <marzhochi@gmail.com>
2024-02-14 21:46:51 +0700

    update

--- app/Http/Controllers/Api/Admin/PetugasController.php
+++ app/Http/Controllers/Api/Admin/PetugasController.php
@@ -53,7 +53,7 @@ class PetugasController extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'data' => $data,
+                'data' => $contents,
             ]);
         } catch (\Exception $e) {
             return response()->json([
